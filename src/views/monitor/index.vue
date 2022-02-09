aacf36cd74b422e2fb8e24276fe3605330a18de7
feng <892751427@qq.com>
2022-02-09 21:25:32 +0800

    feat: 初始化项目;

---
+++ src/views/monitor/index.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="monitor">
+    <button @click="clickHanld">click</button>
+    <ul>
+      <li v-for="item in liList" :key="item.id">{{ item.name }}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      a: 1,
+      liList: [
+        { id: 0, name: 'myy' },
+        { id: 0, name: 'myy' }
+      ]
+    }
+  },
+  computed: {
+    str() {
+      return this.a + 2
+    }
+  },
+
+  methods: {
+    clickHanld() {
+      // Promise.reject('test').then()
+      let body = document.getElementsByTagName('body')[0]
+      let jsNode = document.createElement('script')
+      jsNode.setAttribute('type', 'text/javascript')
+      jsNode.setAttribute('src', 'http://127.0.0.1:8090/hello.js')
+      body.appendChild(jsNode)
+    }
+  }
+}
+</script>
